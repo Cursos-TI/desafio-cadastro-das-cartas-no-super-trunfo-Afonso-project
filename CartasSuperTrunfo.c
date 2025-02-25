302aba54df33bbc9fc769831a1fb42107121315f
Afonso-project <victorafonsofri@gmail.com>
2025-02-25 01:50:25 +0000

    printando informações desafio de cartas supertrunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,36 +3,39 @@
 int main() {
     printf("Desafio Cartas Super Trunfo!\n");
 
+
+    char estado[30];
     char codigo[20];
     int população;
-    double area;
-    double PIB;
+    float area;
+    float PIB;
     int pontos;
     char cidade[50];
 
     printf("Escreva o código da cidade: \n");
-    scanf("%s", codigo);  
-    printf("O código da cidade é: %s\n", codigo);  // Removido o '&'
+    scanf("%s",codigo);  
+    printf("O código da cidade é: %s\n",codigo); 
 
     printf("Digite sua população: \n");
     scanf("%d", &população);  
-    printf("A população é: %d\n", população);  
+    printf("A população é: %d\n",população);  
 
-    printf("Digite o tamanho da área: \n");
-    scanf("%lf", &area);  
-    printf("A área da cidade é: %lf\n", area);  
+    printf("Digite para saber o tamanho da área: \n");
+    scanf("%f", &area);  
+    printf("A área da cidade é: %f\n", area);  
 
     printf("Digite o PIB: \n");
-    scanf("%lf", &PIB);  
-    printf("O PIB da cidade é: %lf\n", PIB);  
+    scanf("%f",&PIB);  
+    printf("O PIB da cidade é: %f\n", PIB);  
 
     printf("Digite os pontos da cidade: \n");
     scanf("%d", &pontos);  
-    printf("Os pontos são: %d\n", pontos); 
+    printf("Os pontos são: %d\n",pontos); 
 
     printf("Digite o nome da cidade: \n");
     scanf("%s", cidade);  
-    printf("O nome da cidade é: %s\n", cidade); 
+    printf("O nome da cidade é: %s\n",cidade); 
 
+    
     return 0;
 }
