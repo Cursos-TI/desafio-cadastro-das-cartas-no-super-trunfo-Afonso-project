297a85da76691a38211e1e63294e3a4c35544ff9
Afonso-project <victorafonsofri@gmail.com>
2025-03-06 20:24:35 +0000

    Nivel aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,9 +3,6 @@
 int main() {
     printf("Desafio Cartas Super Trunfo!\n");
 
-
-    char estado[30];
-    char estado2[30];
     char codigo[20];
     char codigo2[20];
     int populacao;
@@ -18,6 +15,10 @@ int main() {
     int pontos2;
     char cidade[50];
     char cidade2[50];
+    float densidadepopulacional;
+    float densidadepopulacional2;
+    float PIBpercapita;
+    float PIBpercapita2;
 
     printf("[Carta 1] Digite o código da cidade: \n");
     scanf("%s",codigo);  
@@ -27,7 +28,7 @@ int main() {
     scanf("%d", &populacao);  
     printf("A população é: %d\n",populacao);  
 
-    printf("[Carta 1] Digite para saber o tamanho da área: \n");
+    printf("[Carta 1] Digite o tamanho da área: \n");
     scanf("%f", &area);  
     printf("A área da cidade é: %f\n", area);  
 
@@ -42,6 +43,19 @@ int main() {
     printf("[Carta 1] Digite o nome da cidade: \n");
     scanf("%s", cidade);  
     printf("O nome da cidade é: %s\n",cidade); 
+
+    printf("codigo da cidade: %s\n", codigo);
+    printf("população: %d\n", populacao);
+    printf("área: %f\n", area);
+    printf("PIB: %f\n", PIB);
+    printf("pontos: %d\n", pontos);
+    printf("cidade: %s\n", cidade);
+
+    densidadepopulacional = (float) populacao / area;
+    printf("A densidade populacional é de: %f \n", densidadepopulacional);
+
+    PIBpercapita = (float) PIB / populacao;
+    printf("O PIB per capita é de: %f \n", PIBpercapita);
 
     printf("[Carta 2] Digite o código da cidade: \n");
     scanf("%s",codigo2);  
@@ -66,6 +80,19 @@ int main() {
     printf("[Carta 2] Digite o nome da cidade: \n");
     scanf("%s", cidade2);  
     printf("O nome da cidade é: %s\n",cidade2); 
+
+    printf("codigo da cidade: %s\n", codigo2);
+    printf("população: %d\n", populacao2);
+    printf("área: %f\n", area2);
+    printf("PIB: %f\n", PIB2);
+    printf("pontos: %d\n", pontos2);
+    printf("cidade: %s\n", cidade2);
+
+    densidadepopulacional2 = (float) populacao2 / area2;
+    printf("A densidade populacional é de: %f \n", densidadepopulacional2);
+
+    PIBpercapita2 = (float) PIB2 / populacao2;
+    printf("O PIB per capita é de: %f \n", PIBpercapita2);
 
 
     return 0;
